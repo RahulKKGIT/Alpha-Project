c2192d8811bf05bfb0656b8068703b641c39805a
RahulKKGIT <hellorahulkeshri@gmail.com>
2025-09-10 23:46:07 +0530

    Test case 1 changes

--- src/main/java/org/example/Test_Case1.java
+++ src/main/java/org/example/Test_Case1.java
@@ -3,6 +3,6 @@ package org.example;
 public class Test_Case1 {
     public static void main(String[] args){
 
-        System.out.println("Welcome to login page");
+        System.out.println("Welcome to Test Case1");
     }
 }
