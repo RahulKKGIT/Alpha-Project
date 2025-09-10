65e6171e108f7193a6e62599e1b49578b1c59047
RahulKKGIT <hellorahulkeshri@gmail.com>
2025-09-10 23:44:04 +0530

    Login and Test case 1

---
+++ src/Login_Page.java
@@ -0,0 +1,7 @@
+public class Login_Page {
+
+    public static void main(String[] args){
+
+        System.out.println("Welcome to login page");
+    }
+}
---
+++ src/main/java/org/example/Test_Case1.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Test_Case1 {
+    public static void main(String[] args){
+
+        System.out.println("Welcome to login page");
+    }
+}
